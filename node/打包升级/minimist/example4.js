2d4a925a92ac09c73c520d9a6ae2a7c48dcd3a2e
valar_cC <15751165579@163.com>
2018-10-12 18:21:35 +0800

    feat(CLI): 梳理NodeJS中如何给字符串添加样式

---
+++ node/打包升级/minimist/example4.js
@@ -0,0 +1,8 @@
+/**
+ * \x1b[背景颜色编号;字体颜色编号m
+ */
+// process.stdout.write('\n\x1b[41;37m 红色  \x1b[0m 标签\n\n')
+// process.stdout.write('\x1b[42;37m 绿色 \x1b[0m \x1b[4m 标签 \x1b[0m \n\n')
+// process.stdout.write('\x1b[44;37m 蓝色 \x1b[7m 标签\n\n')
+
+process.stdout.write('\x1b[44;37m OK \x1b[0m just do it\n')
