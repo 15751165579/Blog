cfdfda89d516e536f055774d3e6a389b907469b3
valar_cC <15751165579@163.com>
2018-11-12 23:36:26 +0800

    feat(crypto): 了解加密算法中的MD5

---
+++ node/crypto/index.js
@@ -0,0 +1,13 @@
+const crypto = require('crypto')
+
+const hash = crypto.createHash('md5') // MD5 sha256 sha512
+
+hash.update('Hello World')
+
+console.log(hash.digest('hex')) // 1位16进制等于4位二进制 输出16进制
+
+const sha256 = crypto.createHash('sha256')
+
+sha256.update('Hello World')
+
+console.log(sha256.digest('base64')) // 输出base64 
