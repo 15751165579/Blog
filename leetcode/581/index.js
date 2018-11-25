0efc5535fff330c975e61c5519e41128dfbf4d70
valar_cC <15751165579@163.com>
2018-11-25 15:19:57 +0800

    feat(leetcode): 优化581

--- leetcode/581/index.js
+++ leetcode/581/index.js
@@ -1,46 +1,32 @@
 /**
  * 第一种思路：左边的元素要比后面的元素中的最小值还要小，右边的元素要比左边元素中的最大值还要小
+ * 第二种思路 很清奇
  */
 const findUnsortedSubarray = (nums) => {
-  const max = nums.length
-  if (max <= 1) {
+  const len = nums.length
+  if (len <= 1) {
     return 0
   }
-  let count = 0
-  let isOk = true
-  let second = 0
-  for (let i = 0; i < max - 1; i++) {
-    const item = nums[i]
-    for (let j = i + 1; j < max; j++) {
-      if (item > nums[j]) {
-        isOk = false
-        break
-      }
+  let x = 0
+  let y = -1
+  let max = Number.MIN_SAFE_INTEGER
+  let min = Number.MAX_SAFE_INTEGER
+
+  for (let i = 0, j = len - 1; j >= 0; i++, j--) {
+    const pre = nums[i]
+    max = Math.max(max, pre)
+    if (pre !== max) {
+      y = i
     }
-    if (isOk) {
-      count++
-    } else {
-      isOk = true
-      second = i
-      break
+    const next = nums[j]
+    min = Math.min(min, next)
+    if (next !== min) {
+      x = j
     }
   }
-  for (let i = max - 1; i > second; i--) {
-    const item = nums[i]
-    for (let j = i - 1; j >= second; j--) {
-      if (item < nums[j]) {
-        isOk = false
-        break
-      }
-    }
-    if (isOk) {
-      count++
-    } else {
-      break
-    }
-  }
-  return (max - count > 0 ? max - count : 0)
+  return (y - x + 1)
 }
 
 console.log(findUnsortedSubarray([2, 6, 4, 8, 10, 9, 15]), 5)
 console.log(findUnsortedSubarray([2, 1]), 2)
+console.log(findUnsortedSubarray([1,2,3,4]), 0)
