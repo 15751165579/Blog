46759ade53978f4ceeb47d9cd7e9f9dce5050fe6
valar_cC <15751165579@163.com>
2018-11-29 00:45:36 +0800

    feat(leetcode): 每天一道算法题931

--- leetcode/931/index.js
+++ leetcode/931/index.js
@@ -5,11 +5,9 @@ const minFallingPathSum = (A) => {
   const h = A.length
   const w = A.length
   let ans = Number.MAX_SAFE_INTEGER
-
   for (let i = 0; i < w; i++) {
     dfs(A, 1, i, A[0][i])
   }
-
   return ans
 
   function dfs (A, j, i, start) {
@@ -33,8 +31,31 @@ const minFallingPathSum = (A) => {
 
 /**
  * 动态规划
+ * mn
+ * 仍然有优化的地方 以为空间存储dp 
  */
-const minFallingPathSum1 = () => {
+const minFallingPathSum1 = (A) => {
+  const h = A.length
+  if (h <= 0) {
+    return 0
+  }
+  const w = A[0].length
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+      if (i === 0) {
+        continue
+      }
+      let min = A[i - 1][j]
+      if (j - 1 >= 0) {
+        min = Math.min(min, A[i - 1][j - 1])
+      }
+      if (j + 1 < w) {
+        min = Math.min(min, A[i - 1][j + 1])
+      }
+      A[i][j] = min + A[i][j]
+    }
+  }
+  return Math.min.apply(this, A[h - 1])
 }
 
 console.log(minFallingPathSum([[1,2,3],[4,5,6],[7,8,9]]), 12)
