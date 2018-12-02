dab57ceceb36c05bd8de1c774cd82b2f0013cc89
valar_cC <15751165579@163.com>
2018-12-02 12:50:09 +0800

    feat(leetcode): 递归实现全排列

---
+++ leetcode/permutation/demo1.js
@@ -0,0 +1,20 @@
+/**
+ * 递归方法 + in-place的方式
+ */
+const permutation = (A, index, max) => {
+  // 终止条件
+  if (index === max) {
+    console.log(A.slice(0, max))
+  }
+
+  // 在第i位置有几种选择方式
+  for (let i = index; i < A.length; i++) {
+    [A[i], A[index]] = [A[index], A[i]]
+    permutation(A, index + 1, max);
+    [A[i], A[index]] = [A[index], A[i]]
+  }
+}
+
+const A = [1, 2, 3]
+
+permutation(A, 0, 3)
