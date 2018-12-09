a107c0e26e41d57ed77c8a4b5726d049a51cfce9
valar_cC <15751165579@163.com>
2018-12-09 22:45:30 +0800

    feat(leetcode): 每天一道算法题910

--- leetcode/910/index.js
+++ leetcode/910/index.js
@@ -1,5 +1,16 @@
 // 910. Smallest Range II
-const smallestRangeII = () => {
+
+/**
+ * 将每个元素都减去K， 从而将问题转换为将部分元素加上2 * K的问题
+ */
+const smallestRangeII = (A, K) => {
+  A.sort((a, b) => a - b)
+  const max = A.length
+  let ans = A[max - 1] - A[0]
+  for (let i = 0; i < max - 1; i++) {
+    ans = Math.min(ans, Math.max(A[max - 1], A[i] + 2 * K) - Math.min(A[i + 1], A[0] + 2 * K))
+  }
+  return ans
 }
 
 console.log(smallestRangeII([1,3,6], 3), 3)
