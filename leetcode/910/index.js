baa1099f3640b4f4c15b319394c8df9f63d22e6d
valar_cC <15751165579@163.com>
2018-12-09 19:49:33 +0800

    feat(leetcode): 910

---
+++ leetcode/910/index.js
@@ -0,0 +1,7 @@
+// 910. Smallest Range II
+const smallestRangeII = () => {
+}
+
+console.log(smallestRangeII([1,3,6], 3), 3)
+console.log(smallestRangeII([1], 0), 0)
+console.log(smallestRangeII([0, 10], 2), 6)
