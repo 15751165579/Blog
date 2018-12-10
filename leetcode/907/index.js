69c464ceb2c300c5cb7d758f20d738a877e3ca43
valar_cC <15751165579@163.com>
2018-12-10 22:40:15 +0800

    feat(leetcode): :sparkles:每天一道算法题907

--- leetcode/907/index.js
+++ leetcode/907/index.js
@@ -24,3 +24,48 @@ const sumSubarrayMins = A => {
 }
 
 console.log(sumSubarrayMins([3,1,2,4]), 17)
+
+const sumSubarrayMins1 = A => {
+  const MAX = 10 ** 9 + 7
+  const max = A.length
+  let ans = 0
+
+  const left = []
+
+  for (let i = 0; i < max; i++) {
+    let end = i
+    const item = A[i]
+    while (--end >= 0) {
+      if (A[end] >= item) {
+        continue
+      } else {
+        break
+      }
+    }
+    left[i] = i - end - 1
+  }
+  
+  const right = []
+
+  for (let i = 0; i < max; i++) {
+    let end = i
+    const item = A[i]
+    while (++end < max) {
+      if (A[end] > item) {
+        continue
+      } else {
+        break
+      }
+    }
+    right[i] = end - i - 1
+  }
+  for (let i = 0; i < max; i++) {
+    const l = left[i]
+    const r = right[i]
+    ans = (ans + ((l + r + l * r + 1) * A[i]) % MAX) % MAX
+  }
+  return ans
+}
+
+console.log(sumSubarrayMins1([3,1,2,4]), 17)
+console.log(sumSubarrayMins1([71,55,82,55]), 593)
