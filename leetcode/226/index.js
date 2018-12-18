59e085258c44d4ef95df6edc4b63ed8359ac99fd
valar_cC <15751165579@163.com>
2018-12-18 22:57:56 +0800

    feat(leetcode): :sparkles:每天一道算法题

---
+++ leetcode/226/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+const invertTree = (root) => {
+  if (!root) {
+    return root
+  }
+  const temp = root.left
+  root.left = root.right
+  root.right = temp
+  invertTree(root.left)
+  invertTree(root.right)
+  return root
+}
