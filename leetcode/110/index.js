30e5971338454f1b13a6f64f09f854ea66f818b1
valar_cC <15751165579@163.com>
2018-12-21 13:56:10 +0800

    feat(leetcode): :sparkles:每天一道算法题

--- leetcode/110/index.js
+++ leetcode/110/index.js
@@ -2,38 +2,20 @@
  * Balanced Binary Tree
  */
 const isBalanced = function (root) {
+
   if (!root) {
     return true
   }
-  let lh = height(root.left)
-  let rh = height(root.right)
-  return Math.abs(lh - rh) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+
+  const left = height(root.left)
+  const right = height(root.right)
+
+  return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+
   function height (root) {
     if (!root) {
       return 0
     }
     return Math.max(height(root.left), height(root.right)) + 1
-  }
-}
-
-const isBalanced1 = function (root) {
-  if (!root) {
-    return true
-  }
-  let isValid = true
-  height(root)
-  return isValid
-  function height (root) {
-    if (!root) {
-      return 0
-    }
-    const lh = height(root.left)
-    const rh = height(root.right)
-
-    if (Math.abs(lh - rh) > 1) {
-      isValid = false
-       return -1
-    }
-    return Math.max(lh, rh) + 1
   }
 }
