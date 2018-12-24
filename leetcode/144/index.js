25579c6852e3dfa6e057879f8463ce1f51347d7e
valar_cC <15751165579@163.com>
2018-12-24 20:48:02 +0800

    feat(leetcode): :sparkles:每天一道算法题144

---
+++ leetcode/144/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+const preorderTraversal = root => {
+  const ans = []
+  preOrder(root)
+  return ans
+  function preOrder (root) {
+    if (!root) {
+      return 
+    }
+    ans.push(root.val)
+    preOrder(root.left)
+    preOrder(root.right)
+  }
+}
