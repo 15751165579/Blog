ec480a8a6c3d5ecec9c8dce7ef1f87c490c4651e
valar_cC <15751165579@163.com>
2019-01-03 00:45:13 +0800

    feat(leetcode): :sparkles:每天一道算法题95

--- leetcode/95/index.js
+++ leetcode/95/index.js
@@ -1,58 +1,53 @@
-/**
- * Unique Binary Search Tree II
- */
-/**
- * Definition for a binary tree node.
- */
-
-function TreeNode(val) {
-  this.val = val;
-  this.left = this.right = null;
-}
-/**
- * @param {number} n
- * @return {TreeNode[]}
- */
+/* eslint-disable */
 var generateTrees = function(n) {
-  return generateTree(1, n)
-  function generateTree (min, max) {
-    const trees = []
-    
+
+  return help(1, n)
+
+  function help (min, max) {
+    const result = []
     if (min > max) {
-      return trees
+      return result
     }
 
     for (let mid = min; mid <= max; mid++) {
-      const leftChild = generateTree(min, mid - 1)
-      const rightChild = generateTree(mid + 1, max)
-      if (leftChild.length === 0 && rightChild.length === 0) {
+      // 得到左右子树
+      const leftTree = help(min, mid - 1)
+      const rightTree = help(mid + 1, max)
+
+      const llen = leftTree.length
+      const rlen = rightTree.length
+
+      if (llen === 0 && rlen === 0) {
+        // 左右子树为空的情况
         const root = new TreeNode(mid)
-        trees.push(root)
-      } else if (leftChild.length === 0) {
-        for (let i = 0; i < rightChild.length; i++) {
+        result.push(root)
+      } else if (llen === 0) {
+        // 只有右子树的情况
+        for (let i = 0; i < rlen; i++) {
           const root = new TreeNode(mid)
-          root.right = rightChild[i]
-          trees.push(root)
+          root.right = rightTree[i]
+          result.push(root)
         }
-      } else if (rightChild.length === 0) {
-        for (let i = 0; i < leftChild.length; i++) {
+      } else if (rlen === 0) {
+        // 只有左子树的情况
+        for (let i = 0; i < llen; i++) {
           const root = new TreeNode(mid)
-          root.left = leftChild[i]
-          trees.push(root)
+          root.left = leftTree[i]
+          result.push(root)
         }
       } else {
-        for (let i = 0; i < leftChild.length; i++) {
-          for (let j = 0; j < rightChild.length; j++) {
+        // 左右子树组合的情况
+        for (let i = 0; i < llen; i++) {
+          for (let j = 0; j < rlen; j++) {
             const root = new TreeNode(mid)
-            root.left = leftChild[i]
-            root.right = rightChild[j]
-            trees.push(root)
+            root.left = leftTree[i]
+            root.right = rightTree[j]
+            result.push(root)
           }
         }
       }
     }
-    return trees
+
+    return result
   }
 }
-
-console.log(generateTrees(3))
