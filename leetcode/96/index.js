6d58d7c55432a4c6ed410cc94b28748fce226e95
valar_cC <15751165579@163.com>
2019-01-03 00:06:53 +0800

    feat(leetcode): :sparkles:每天一道算法题96

--- leetcode/96/index.js
+++ leetcode/96/index.js
@@ -2,6 +2,14 @@
  * Unique Binary Search Tree
  * 
  * dp
+ * 
+ * 状态 dp[n] 表示n个节点生成BST的数量
+ * 
+ * 初始状态 dp[0] = 1 dp[1] = 1
+ * 
+ * 
+ * 递推公式 dp[n] = dp[1] * dp[n - 1] + dp[2] * dp[n - 2] ....
+ * 
  */
 
 const numTrees = function (n) {
@@ -9,9 +17,9 @@ const numTrees = function (n) {
   dp[0] = 1
   dp[1] = 1
 
-  for (let i = 2; i <= n; i++) {
-    for (let j = 1; j <= i; j++) {
-      dp[i] += dp[j - 1] * dp[i - j]
+  for (let i = 2;i <= n; i ++) {
+    for (let j = 0; j < i; j++) {
+      dp[i] += dp[j] * dp[i - j - 1]
     }
   }
   return dp[n]
