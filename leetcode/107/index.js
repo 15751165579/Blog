c19fc447c14e47904d9fa1b747e88f8f27145a14
valar_cC <15751165579@163.com>
2019-01-03 21:37:35 +0800

    feat(leetcode): :sparkles:每天一道算法题107

--- leetcode/107/index.js
+++ leetcode/107/index.js
@@ -1,61 +1,35 @@
-/**
- * Binary Tree Level Order Traversal
- */
-
-function TreeNode (val) {
-  this.val = val
-  this.right = this.left = null
-}
-const levelOrderBottom = function (root) {
-  const result = []
-  if (root === null) {
-    return result
+/* eslint-disable */
+const levelOrderBottom = root => {
+  if (!root) {
+    return []
   }
-  const q = []
-  q.push(root)
-  result.push([root.val])
-  while (q.length > 0) {
-    const len = q.length
+  const queue = [root]
+
+  const ans = []
+
+  while (queue.length) {
+
     const temp = []
-    for (let i = 0; i < len; i++) {
-      const item = q.shift()
-      const left = item.left
-      const right = item.right
-      if (left !== null) {
-        temp.push(left.val)
-        q.push(left)
+    const size = queue.length
+
+    for (let i = 0; i < size; i++) {
+      const item = queue.pop()
+
+      if (item) {
+        temp.push(item.val)
+
+        if (item.left) {
+          queue.unshift(item.left)
+        }
+  
+        if (item.right) {
+          queue.unshift(item.right)
+        }
       }
-      if (right !== null) {
-        temp.push(right.val)
-        q.push(right)
-      }
     }
-    if (temp.length > 0) {
-      result.unshift(temp)
-    }
+
+    ans.unshift(temp)
   }
-  return result
-}
 
-const levelOrderBottom1 = function (root) {
-  const result = []
-  _levelOrderBottom(root, result, 0)
-  return result.reverse()
-  function _levelOrderBottom (root, result, dep) {
-    if (!root) {
-      return
-    }
-    let temp = null
-    if (result[dep]) {
-      temp = result[dep]
-    } else {
-      temp = []
-      result[dep] = temp
-    }
-
-    temp.push(root.val)
-
-    _levelOrderBottom(root.left, result, ++dep)
-    _levelOrderBottom(root.right, result, dep)
-  }
+  return ans
 }
