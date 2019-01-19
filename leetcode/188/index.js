53f0ce8a3fc106c3a3a34962f2ecd596d49a0de4
valar_cC <15751165579@163.com>
2019-01-20 01:45:52 +0800

    feat(leetcode): :sparkles:第一次优化188

--- leetcode/188/index.js
+++ leetcode/188/index.js
@@ -41,3 +41,38 @@ const maxProfit = (k, prices) => {
 
   return dp[k][max]
 }
+
+const maxProfit1 = (k, prices) => {
+  const max = prices.length
+  if (max < 2) {
+    return 0
+  }
+
+  const dp = []
+  
+  // 初始状态
+  for (let i = 0; i <= k; i++) {
+    dp[i] = [0]
+    if (i === 0) {
+      for (let j = 0; j < max; j++) {
+        dp[i][j] = 0
+      }
+    }
+  }
+  for (let i = 1; i <= k; i++) {
+    let maxDiff = Number.MIN_SAFE_INTEGER 
+    for (let j = 1; j < max; j++) {
+      let max = dp[i][j - 1]
+
+      maxDiff = Math.max(maxDiff, dp[i - 1][j - 1] - prices[j - 1])
+
+      max = Math.max(max, prices[j] + maxDiff)
+
+      dp[i][j] = max
+    }
+  }
+  console.log(dp)
+  return dp[k][max - 1]
+}
+
+console.log(maxProfit1(3, [2, 5, 7, 1, 4, 3, 1, 3]))
