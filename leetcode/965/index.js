8268da9f1132540938517480b9d1f82de05740d3
valar_cC <15751165579@163.com>
2019-01-20 18:33:13 +0800

    feat(leetcode): :sparkles:二刷965

--- leetcode/965/index.js
+++ leetcode/965/index.js
@@ -24,3 +24,40 @@ const isUnivalTree = root => {
     help(root.right)
   }
 }
+
+const isUnivalTree1 = root => {
+  if (!root) {
+    return true
+  }
+
+  const queue = [root]
+  const val = root.val
+
+  while (queue.length) {
+    const max = queue.length
+
+    for (let i = 0; i < max; i++) {
+      const item = queue.pop()
+
+      if (item) {
+        if (!checkSame(item, val)) {
+          return false
+        }
+        if (item.left && !checkSame(item.left, val)) {
+          return false
+        }
+        if (item.right && !checkSame(item.right, val)) {
+          return false
+        }
+
+        item.left && queue.unshift(item.left)
+        item.right && queue.unshift(item.right)
+      }
+    }
+  }
+  return true
+
+  function checkSame (root, val) {
+    return root.val === val
+  }
+}
