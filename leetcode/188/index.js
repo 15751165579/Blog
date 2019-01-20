f5e7285a28e4659d529a26adbcaa5aa26aae36a7
valar_cC <15751165579@163.com>
2019-01-20 18:13:02 +0800

    feat(leetcode): :sparkles:解决188中testcase的问题

--- leetcode/188/index.js
+++ leetcode/188/index.js
@@ -75,4 +75,49 @@ const maxProfit1 = (k, prices) => {
   return dp[k][max - 1]
 }
 
-console.log(maxProfit1(3, [2, 5, 7, 1, 4, 3, 1, 3]))
+const maxProfit2 = (k, prices) => {
+  const max = prices.length
+  if (max < 2) {
+    return 0
+  }
+
+  if (k >= max / 2) {
+    return help(prices, max)
+  }
+
+  let dp = []
+  
+  // 初始状态
+  for (let i = 0; i < max; i++) {
+    dp[i] = 0
+  }
+  for (let i = 1; i <= k; i++) {
+    let maxDiff = Number.MIN_SAFE_INTEGER
+    let temp = [0]
+    for (let j = 1; j < max; j++) {
+      let max = temp[j - 1]
+
+      maxDiff = Math.max(maxDiff, dp[j - 1] - prices[j - 1])
+
+      max = Math.max(max, prices[j] + maxDiff)
+
+      temp[j] = max
+    }
+    dp = temp
+  }
+  return dp[max - 1]
+
+  function help (prices, max) {
+    const hold = [-prices[0]]
+    const sell = [0]
+
+    for (let i = 1; i < max; i++) {
+      sell[i] = Math.max(sell[i - 1], hold[i - 1] + prices[i])
+      hold[i] = Math.max(hold[i - 1], sell[i - 1] - prices[i])
+    }
+    return sell[max - 1]
+  }
+}
+
+console.log(maxProfit1(2, [3,3,5,0,0,3,1,4]))
+console.log(maxProfit2(2, [3,3,5,0,0,3,1,4]))
