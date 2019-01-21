3da510640b10900cfec421fe2a8421b2d9afa0ce
valar_cC <15751165579@163.com>
2019-01-21 22:02:41 +0800

    feat(leetcode): :sparkles:优化653解法

--- leetcode/653/index.js
+++ leetcode/653/index.js
@@ -27,3 +27,25 @@ const findTarget = (root, k) => {
     help(root.right)
   }
 }
+
+const findTarget1 = (root, k) => {
+  let ans = false
+  const s = new Set()
+
+  inOrder(root)
+  return ans
+  function inOrder (root) {
+    if (!root) {
+      return
+    }
+
+    inOrder(root.left)
+    const rest = k - root.val
+    if (s.has(rest)) {
+      ans = true
+      return
+    }
+    s.add(root.val)
+    inOrder(root.right)
+  }
+}
