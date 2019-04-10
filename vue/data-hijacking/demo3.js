32d67e2edcb2bc87043299d1ed8ef373f22540f4
valar_cC <15751165579@163.com>
2019-04-10 18:30:07 +0800

    feat(data-hijacking): :sparkles:解决存在原始 getter setter 的情况

---
+++ vue/data-hijacking/demo3.js
@@ -0,0 +1,85 @@
+/**
+ * 数据双向绑定
+ * 
+ * 
+ * 
+ *       data
+ *        
+ *       getter -------> 收集依赖 
+ *                                  Watcher -------> 更新视图
+ *       setter -------> 通知变化 
+ * 
+ * 
+ * 数据劫持 + 发布订阅
+ */
+
+// 二、自身设置了 getter setter
+function defineReactive (obj, key) {
+  let val = obj[key]
+
+  const desc = Object.getOwnPropertyDescriptor(obj, key)
+  if (desc && desc.configurable === false) {
+    return
+  }
+
+  // 拿到之前的 getter setter
+  const getter = desc && desc.get
+  const setter = desc && desc.set
+
+  Object.defineProperty(obj, key, {
+    get () {
+      const value = getter ? getter.call(obj) : val
+      console.log(' === 收集依赖 === ')
+      console.log(' 当前值为：' + value)
+      return value
+    },
+    set (newValue) {
+      const value = getter ? getter.call(obj) : val
+      // 如果值相同则没必要更新 === 的坑点 NaN!!!!
+      if (newValue === value || (value !== value && newValue !== newValue)) {
+        return
+      }
+
+      if (getter && !setter) {
+        // 这种情况 不可以赋值
+        return
+      }
+
+      console.log(' === 通知变更 === ')
+      console.log(' 当前值为：' + newValue)
+
+      if (setter) {
+        // 调用原始的 setter 方法
+        setter.call(obj, newValue)
+      } else {
+        val = newValue
+      }
+    }
+  })
+}
+
+const student = {}
+
+function defineReactiveOrigin (obj, key) {
+  let _val = 'xiaoming'
+  Object.defineProperty(obj, key, {
+    get () {
+      console.log(' *** 原始的 getter *** ')
+      return _val
+    },
+    set (newValue) {
+      console.log(' *** 原始的 setter *** ')
+      _val = newValue
+    },
+    enumerable: true,
+    configurable: true
+  })
+}
+
+defineReactiveOrigin(student, 'name')
+
+defineReactive(student, 'name')
+
+student.name
+student.name = 'xiaohong'
+student.name
