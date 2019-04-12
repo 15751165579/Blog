292641f923d3c13ad8e244c9aecf1d4151664170
valar_cC <15751165579@163.com>
2019-04-12 14:23:59 +0800

    feat(data-hijacking): :sparkles:数组的劫持

--- vue/data-hijacking/demo4.js
+++ vue/data-hijacking/demo4.js
@@ -1,35 +1,54 @@
 // 四、数组的劫持
 
-const methods = [
-  'push',
-  'pop',
-  'shift',
-  'unshift',
-  'sort',
-  'reverse',
-  'splice'
-]
-console.log(methods)
-
 const arr = [1, 2, 3]
 
-// arr [[Prototype]] ----> Array.prototype
+function hijackingArray (arr) {
+  const methods = [
+    'push',
+    'pop',
+    'shift',
+    'unshift',
+    'sort',
+    'reverse',
+    'splice'
+  ]
+  const arrayProto = Array.prototype
+  const injackingPrototype = Object.create(arrayProto) // 原型
 
-const arrayProto = Array.prototype
+  methods.forEach(method => {
+    const originArrayMethod = arrayProto[method]
+    injackingPrototype[method] = function (...args) {
+      console.log(`劫持原生的 ${method} 方法`)
+      const result = originArrayMethod.apply(this, args)
 
-const injackingPrototype = Object.create(arrayProto)
+      // 一、对于传入的参数同样要劫持
+      let inserted
+      switch (method) {
+        case 'push':
+        case 'unshift':
+          inserted = args
+          break
+        case 'splice':
+          inserted = args.slice(2)
+          break
+      }
+      if (inserted) {
+        // ob.observeArray(inserted)
+        // 继续劫持
+      }
+      return result
+    }
+  })
+  /**
+   * 二、修改原型
+   * 古老的 __proto__
+   * 
+   * ES6 setPrototypeOf
+   */
+  return Object.setPrototypeOf(arr, injackingPrototype)
+}
 
-
-methods.forEach(method => {
-  injackingPrototype[method] = function () {
-    console.log(`劫持原生的 ${method} 方法`)
-    return arrayProto[method].apply(this, arguments)
-  }
-})
-
-// arr [[Prototype]] ----> injackingPrototype
-
-Object.setPrototypeOf(arr, injackingPrototype)
+hijackingArray(arr)
 
 
 arr.push(4)
@@ -37,4 +56,4 @@ console.log(arr)
 arr.unshift(0)
 console.log(arr)
 arr.sort()
-console.log(arr)
+console.log(arr.shift())
