37a13f5b512843c2dacb8b6c296ac007fefc148c
valar_cC <15751165579@163.com>
2019-05-09 20:20:25 +0800

    feat(rxjs5.0): :sparkles:理解同步数据流

---
+++ rxjs/5.0/examples/demo9.js
@@ -0,0 +1,29 @@
+// 同步数据流
+// create 本质上就是 Observable 构造函数的无 new 写法
+// of
+// range
+// generate
+const Rx = require('rxjs/Rx')
+const source1$ = Rx.Observable.of(1, 2, 3, 4)
+source1$.subscribe(console.log)
+
+const source2$ = Rx.Observable.range(1, 10)
+source2$.subscribe(console.log)
+
+// generate 是 for 声明式的处理
+const source3$ = Rx.Observable.generate(
+  2,
+  val => val < 10,
+  val => val + 2,
+  val => val * val,
+)
+source3$.subscribe(console.log)
+
+// repeat 产生了 10 新的 Obserable
+const source4$ = Rx.Observable.of(1, 2, 3)
+source4$.repeat(10).subscribe(console.log)
+
+// empty 创建一个直接完结的 Observable 对象
+// throw 创建一个 Observale 对象，直接抛错
+// never 创建一个什么都不做的 Observale 对象
+
